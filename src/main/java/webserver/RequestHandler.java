765df0e1f71540865515e59e5f935aa1ac9ffb9c
soohoonlee <jjinimania@gmail.com>
2018-06-28 14:51:27 +0900

    RequestMapping 추가   - 요청 URL에 대해서 해당하는 Controller 반환 RequestHandler   - 요청 URL에 대한 Controller을 찾아서 해당 작업 위임

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,21 +1,19 @@
 package webserver;
 
-import db.DataBase;
+import controller.Controller;
 import http.HttpRequest;
 import http.HttpResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Collection;
-import java.util.Map;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
+
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String INDEX_HTML = "/index.html";
 
     private Socket connection;
 
@@ -23,6 +21,7 @@ public class RequestHandler extends Thread {
         this.connection = connectionSocket;
     }
 
+    @Override
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
@@ -30,82 +29,24 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             final HttpRequest httpRequest = new HttpRequest(in);
             final HttpResponse httpResponse = new HttpResponse(out);
-            final String path = getDefaultPath(httpRequest.getPath());
 
-            if ("/user/create".equals(path)) {
-                createUser(httpRequest, httpResponse);
-            } else if ("/user/login".equals(path)) {
-                login(httpRequest, httpResponse);
-            } else if ("/user/list".equals(path)) {
-                listUser(httpRequest, httpResponse);
-            } else {
+            final Controller controller = RequestMapping.getController(httpRequest.getPath());
+            if (controller == null) {
+                final String path = getDefaultPath(httpRequest.getPath());
                 httpResponse.forward(path);
+            } else {
+                controller.service(httpRequest, httpResponse);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void listUser(final HttpRequest httpRequest, final HttpResponse httpResponse)
-        throws IOException {
-        if (!isLogin(httpRequest.getHeader("Cookie"))) {
-            httpResponse.sendRedirect("/user/login.html");
-            return;
-        }
-        Collection<User> users = DataBase.findAll();
-        StringBuilder sb = new StringBuilder();
-        sb.append("<table border='1'>");
-        for (User user : users) {
-            sb.append("<tr>");
-            sb.append("<td>" + user.getUserId() + "</td>");
-            sb.append("<td>" + user.getName() + "</td>");
-            sb.append("<td>" + user.getEmail() + "</td>");
-            sb.append("</tr>");
-        }
-        sb.append("</table>");
-        httpResponse.forwardBody(sb.toString());
-    }
-
-    private void login(final HttpRequest httpRequest, final HttpResponse httpResponse)
-        throws IOException {
-        User user = DataBase.findUserById(httpRequest.getParameter("userId"));
-        if (user != null) {
-            if (user.login(httpRequest.getParameter("password"))) {
-                httpResponse.addHeader("Set-Cookie", "logined=true");
-                httpResponse.sendRedirect("/index.html");
-            } else {
-                httpResponse.sendRedirect("/user/login_failed.html");
-            }
-        } else {
-            httpResponse.sendRedirect("/user/login_failed.html");
-        }
-    }
-
-    private void createUser(final HttpRequest httpRequest, final HttpResponse httpResponse)
-        throws IOException {
-        final User user = new User(httpRequest.getParameter("userId"),
-            httpRequest.getParameter("password"), httpRequest.getParameter("name"),
-            httpRequest.getParameter("email"));
-        log.debug("user : {}", user);
-        DataBase.addUser(user);
-        httpResponse.sendRedirect("/index.html");
-    }
-
     private String getDefaultPath(String path) {
         if (path.equals("/")) {
-            path = "/index.html";
+            path = INDEX_HTML;
         }
         return path;
-    }
-
-    private boolean isLogin(String line) {
-        String[] headerTokens = line.split(":");
-        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
-        String value = cookies.get("logined");
-        if (value == null) {
-            return false;
-        }
-        return Boolean.parseBoolean(value);
     }
 
 }
